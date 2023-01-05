04fabb723a390af7b7b820f73a4eb1dfa3660501
lexandfr <alexfloresrayme@gmail.com>
2023-01-04 23:01:49 -0500

    deploy config

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import TaskForm from "./components/TaskForm.jsx";
 
 function App() {
   return (
-    <main className="bg-zinc-900 h-full">
+    <main className="bg-zinc-900 h-screen">
       <h1 className="text-3xl font-bold text-white text-center p-8">Task Manager</h1>
       <div className="container mx-auto p-10">
         <TaskForm />
